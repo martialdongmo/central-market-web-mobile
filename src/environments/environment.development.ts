c82a9457b1bf717754172ce46edeccb17d1ac72c
sidofdountio <sidofdountio406@gmail.com>
2026-05-21 11:16:39 +0100

    fix cors error

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  AUTH_API_URL: 'http://localhost:9000',
-  USER_API_URL: 'http://localhost:9000/api/v1/bis/users',
+  AUTH_API_URL: 'http://localhost:9009',
+  USER_API_URL: 'http://localhost:9009/api/v1/bis/users',
   CATALOG_API_URL: "http://localhost:9005/api/v1/bis/catalog",
   categoryEndpoint: 'http://localhost:9002/api/v1/bis/categories',
   customersEndpoint: 'http://localhost:9003/api/v1/bis/customers',
