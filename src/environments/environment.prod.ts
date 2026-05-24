4fd43548595af27a4089e1b43d96b05a934e56b9
sidofdountio <sidofdountio406@gmail.com>
2026-05-24 17:51:53 +0100

    fix reseve proxy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
-  AUTH_API_URL: 'https://kapexpert.cloud:9010',
-  USER_API_URL: 'https://kapexpert.cloud:9010/api/v1/bis/users',
-  CATALOG_API_URL: "https://kapexpert.cloud:9010/api/v1/bis/catalog",
-  categoryEndpoint: 'https://kapexpert.cloud:9010/api/v1/bis/categories',
-  customersEndpoint: 'https://kapexpert.cloud:9010/api/v1/bis/customers',
-  orderEndpoint: 'https://kapexpert.cloud:9010/api/v1/bis/orders',
-   paymentEndpoint: 'https://kapexpert.cloud:9010/api/v1/bis/payments',
+  AUTH_API_URL: 'https://kapexpert.cloud:9009',
+  USER_API_URL: 'https://kapexpert.cloud:9009/api/v1/bis/users',
+  CATALOG_API_URL: "https://kapexpert.cloud:9009/api/v1/bis/catalog",
+  categoryEndpoint: 'https://kapexpert.cloud:9009/api/v1/bis/categories',
+  customersEndpoint: 'https://kapexpert.cloud:9009/api/v1/bis/customers',
+  orderEndpoint: 'https://kapexpert.cloud:9009/api/v1/bis/orders',
+   paymentEndpoint: 'https://kapexpert.cloud:9009/api/v1/bis/payments',
   redirectUri: 'https://kapexpert.cloud:3000/callback'
 };
